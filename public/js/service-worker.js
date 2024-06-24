700008163a60ca0ca9eaa30b2168b1898e07f505
ahed666 <ahed0naser0eddin@gmail.com>
2024-06-24 17:46:25 +0400

    edit test

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'offline-cache-v5';
-const OFFLINE_URL = '/index.html'; // The URL of the web page to cache
+const OFFLINE_URL = '/device-player'; // The URL of the web page to cache
 
 // Install event: caching the offline page
 self.addEventListener('install', event => {
